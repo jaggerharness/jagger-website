9e41abdeacd93c2785caa199a782286d7dea7e89
Jagger <jagger@aceonetechnologies.com>
2022-10-11 21:06:14 -0500

    init push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <header className="flex flex-col min-h-screen align-middle justify-center items-center bg-ws-background bg-fixed">
+        <h1 className="text-4xl text-center font-bold text-ws-green font-mono">
+          Hello world!
+        </h1>
+        <p className="text-5xl text-white align-middle w-fit font-bold font-mono hover:animate-pulse">
+          Welcome to my website
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="text-xl text-ws-light-gray hover:text-ws-lime-green text-center font-bold font-mono mt-3">
+          // I'm Jagger
+        </p>
+        <p className="text-xl text-ws-light-gray hover:text-ws-lime-green text-center font-bold font-mono">
+          // Software Engineer
+        </p>
+        <p className="text-xl text-ws-light-gray hover:text-ws-lime-green text-center font-bold font-mono">
+          // Gamer
+        </p>
       </header>
     </div>
   );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <h1 className="text-3xl font-bold underline">
+  //         Hello world!
+  //       </h1>
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'ws-green': '#61892F',
+        'ws-lime-green': '#86C232',
+        'ws-black': '#222629',
+        'ws-dark-gray': '#474B4F',
+        'ws-light-gray': '#6B6E70',
+      },
+      backgroundImage: {
+        'ws-background': "url('../public/images/ws-bg.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
