16bebba9a3b5ea752fc8e79d9ac8e4d049776b80
jaggerharness <jaggerharness5@gmail.com>
2022-11-12 23:01:39 -0600

    UI modifications - Home Page

--- src/App.js
+++ src/App.js
@@ -6,19 +6,11 @@ function App() {
     <div>
       <header className="flex flex-col min-h-screen align-middle justify-center items-center bg-ws-background bg-fixed">
         <h1 className="text-4xl text-center font-bold text-ws-green font-mono">
-          Hello world!
+          Hello, world!
         </h1>
         <p className="text-5xl text-white align-middle w-fit font-bold font-mono hover:animate-pulse">
-          Welcome to my website
+          I'm Jagger Harness
         </p>
-        <p className="text-xl text-ws-light-gray hover:text-ws-lime-green text-center font-bold font-mono mt-3">
-          // Jagger Harness
-        </p>
-        <p className="text-xl text-ws-light-gray hover:text-ws-lime-green text-center font-bold font-mono">
-          // Software Engineer
-        </p>
-        <button class="text-xl text-center font-bold font-mono text-ws-light-gray hover:text-ws-lime-green hover:line-through transition duration-150 ease-in-out"
-          data-bs-toggle="tooltip" title="I wish">// Professional Gamer</button>
       </header>
     </div>
   );
