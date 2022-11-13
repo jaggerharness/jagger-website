7a9ea114761c8ef2a19a3dead93a5de5dac97625
jaggerharness <jaggerharness5@gmail.com>
2022-11-12 23:26:51 -0600

    React hooks, state implementations

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <header className="flex flex-col min-h-screen align-middle justify-center items-center bg-ws-background bg-fixed">
+      <div className="flex flex-col min-h-screen align-middle justify-center items-center bg-ws-background bg-fixed">
         <h1 className="text-4xl text-center font-bold text-ws-green font-mono">
           Hello, world!
         </h1>
-        <p className="text-5xl text-white align-middle w-fit font-bold font-mono hover:animate-pulse">
+        <div className="text-5xl text-white align-middle pb-5 w-fit font-bold font-mono hover:animate-pulse">
           I'm Jagger Harness
-        </p>
-      </header>
+        </div>
+        <div>
+          <button
+            onClick={() => setCount(count + 1, console.log(count))}
+            class="bg-ws-green hover:bg-ws-lime-green text-white font-bold py-2 px-4 rounded"
+          >
+            Projects
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
