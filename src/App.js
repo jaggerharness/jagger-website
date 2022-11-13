487d81d60a4c5b74503d99dbfebf33a2b776f0cb
jaggerharness <jaggerharness5@gmail.com>
2022-11-13 15:13:07 -0600

    Connection to backend / fetch API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [connection, setConnection] = useState("Failed");
+
+  useEffect(() => {
+    const url = "http://localhost:9000/connection";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setConnection(data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
       <div className="flex flex-col min-h-screen align-middle justify-center items-center bg-ws-background bg-fixed">
+        <div className="text-xl font-bold text-white font-mono">
+          Connection to API:
+          <span className={connection === 'Success' ? "text-ws-lime-green" : "text-red-600"} > {connection}</span>
+        </div>
         <h1 className="text-4xl text-center font-bold text-ws-green font-mono">
           Hello, world!
         </h1>
