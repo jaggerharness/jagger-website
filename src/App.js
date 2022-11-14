d5df87932ba0fd5c922751cd7f542cc318cd965b
jaggerharness <jaggerharness5@gmail.com>
2022-11-13 20:05:36 -0600

    UI updates

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   useEffect(() => {
     const url = "http://localhost:9000/connection";
 
-    const fetchData = async () => {
+    const checkAPIConnection = async () => {
       try {
         const response = await fetch(url);
         const data = await response.json();
@@ -18,29 +18,37 @@ function App() {
       }
     };
 
-    fetchData();
+    checkAPIConnection();
   }, []);
 
   return (
     <div>
-      <div className="flex flex-col min-h-screen align-middle justify-center items-center bg-ws-background bg-fixed">
-        <div className="text-xl font-bold text-white font-mono">
-          Connection to API:
-          <span className={connection === 'Success' ? "text-ws-lime-green" : "text-red-600"} > {connection}</span>
-        </div>
-        <h1 className="text-4xl text-center font-bold text-ws-green font-mono">
-          Hello, world!
-        </h1>
-        <div className="text-5xl text-white align-middle pb-5 w-fit font-bold font-mono hover:animate-pulse">
-          I'm Jagger Harness
-        </div>
-        <div>
-          <button
-            onClick={() => setCount(count + 1, console.log(count))}
-            class="bg-ws-green hover:bg-ws-lime-green text-white font-bold py-2 px-4 rounded"
+      <div className="flex flex-col min-h-screen bg-ws-background bg-fixed">
+        <div className="text-xl font-bold text-white text-right font-mono pr-2 pt-2">
+          {`Connection to API: `}
+          <span
+            className={
+              connection === "Success" ? "text-ws-lime-green" : "text-red-600"
+            }
           >
-            Projects
-          </button>
+            {connection}
+          </span>
+        </div>
+        <div className="flex flex-col justify-center min-h-screen align-center items-center">
+          <h1 className="text-4xl font-bold text-ws-green font-mono">
+            Hello, world!
+          </h1>
+          <div className="text-5xl text-white align-middle pb-5 w-fit font-bold font-mono hover:animate-pulse">
+            I'm Jagger Harness
+          </div>
+          <div>
+            <button
+              onClick={() => setCount(count + 1, console.log(count))}
+              class="bg-ws-green hover:bg-ws-lime-green text-white font-bold py-2 px-4 rounded"
+            >
+              Projects
+            </button>
+          </div>
         </div>
       </div>
     </div>
