9916541a0321e79e14ca3f36576ac4a18d706696
Jagger <jagger@aceonetechnologies.com>
2022-11-14 15:07:23 -0600

    Project Page no longer broken - multiple API calls in useEffect

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -4,35 +4,33 @@ import Project from "../components/Project.js";
 
 const ProjectsPage = (props) => {
   const [connection, setConnection] = useState("Failed");
-  const [projects, setProjects] = useState("");
-  const projectList = projects.map(project => <Project data={project}/>);
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     const url = "http://localhost:8080/connection";
     const url2 = "http://localhost:8080/fetchProjects"
 
-    // const checkAPIConnection = async () => {
-    //   try {
-    //     const response = await fetch(url);
-    //     const data = await response.json();
-    //     setConnection(data);
-    //   } catch (error) {
-    //     console.log("error", error);
-    //   }
-    // };
-
-    const fetchProjects = async () => {
+    const checkAPIConnection = async () => {
       try {
-        const response = await fetch(url2);
+        const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
-        setProjects(data);
+        setConnection(data);
       } catch (error) {
         console.log("error", error);
       }
     };
 
-    // checkAPIConnection();
+    const fetchProjects = async () => {
+      try {
+        const response = await fetch(url2);
+        const data = await response.json().then(element => setProjects(element.projects));
+        // setProjects(data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    checkAPIConnection();
     fetchProjects();
   }, []);
 
@@ -57,7 +55,9 @@ const ProjectsPage = (props) => {
         <div className="text-4xl text-white align-top pl-2 pt-2 w-fit font-bold font-mono">
           Projects (just for fun)
         </div>
-        {/* {projectList} */}
+        {projects.map(project => {
+          return <Project data={project} />
+        })}
       </div>
     </div>
   );
