d2f099f10649dc8de2ae1e5fc4c85abc6a39fe9a
jaggerharness <jaggerharness5@gmail.com>
2022-12-20 20:42:58 -0600

    Minor cleanup / formatting

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -22,18 +22,27 @@ const NavigationBar = () => {
 
   return (
     <div className="flex flex-row bg-ws-light-gray p-2">
-      <Link to="/" className={`${window.location.pathname === '/' ? 'underline' : ''} text-xl font-bold text-white font-mono basis-1/3 text-center md:ml-2 md:basis-0 hover:underline underline-offset-4 decoration-ws-lime-green`}>
+      <Link
+        to="/"
+        className={`${
+          window.location.pathname === "/" ? "underline" : ""
+        } text-xl font-bold text-white font-mono basis-1/3 text-center md:ml-2 md:basis-0 hover:underline underline-offset-4 decoration-ws-lime-green`}
+      >
         Home
       </Link>
       <Link
         to="/about-me"
-        className={`${window.location.pathname === '/about-me' ? 'underline' : ''} text-xl font-bold text-white font-mono basis-1/3 text-center md:whitespace-nowrap mr-4 md:ml-10 md:mr-0 md:basis-0 hover:underline underline-offset-4 decoration-ws-lime-green`}
+        className={`${
+          window.location.pathname === "/about-me" ? "underline" : ""
+        } text-xl font-bold text-white font-mono basis-1/3 text-center md:whitespace-nowrap mr-4 md:ml-10 md:mr-0 md:basis-0 hover:underline underline-offset-4 decoration-ws-lime-green`}
       >
         About Me
       </Link>
       <Link
         to="/projects"
-        className={`${window.location.pathname === '/projects' ? 'underline' : ''} text-xl font-bold text-white font-mono basis-1/3 text-center md:ml-10 md:basis-0 hover:underline underline-offset-4 decoration-ws-lime-green`}
+        className={`${
+          window.location.pathname === "/projects" ? "underline" : ""
+        } text-xl font-bold text-white font-mono basis-1/3 text-center md:ml-10 md:basis-0 hover:underline underline-offset-4 decoration-ws-lime-green`}
       >
         Projects
       </Link>
